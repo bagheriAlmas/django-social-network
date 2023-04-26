d46a1b573649ee47b1f4eb14ac66430640bba267
mahdibagheri <bagheri.almas@gmail.com>
2023-04-26 23:04:03 +0330

    add write_only attribute for password1 and password2

--- accounts/serialisers.py
+++ accounts/serialisers.py
@@ -7,8 +7,8 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators=[UniqueValidator(User.objects.all())])
-    password1 = serializers.CharField(required=True)
-    password2 = serializers.CharField(required=True)
+    password1 = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
     '''we can define first_name and last_name here. but i prefer to define these in extera_kwargs'''
 
     # first_name = serializers.CharField(required=False)
@@ -33,12 +33,18 @@ class RegisterSerializer(serializers.ModelSerializer):
         return super(RegisterSerializer, self).validate(attrs)
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
+            password=validated_data['password1']
         )
 
-        user.set_password(validated_data['password1'])
-        user.save()
+        ''' if we used User.objects.create , we have to use below lines
+            to convert raw password to hash and we need to remove password from create method. 
+            but if we use User.objects.create_user, it's no longer neeeded to define below lines.
+        '''
+        # user.set_password(validated_data['password1'])
+        # user.save()
+        return user
