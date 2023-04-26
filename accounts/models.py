6dd0d88d7a041b47888f6898c602f1fe59a9e71e
mahdibagheri <bagheri.almas@gmail.com>
2023-04-26 21:48:10 +0330

    create models Country, Profile and Device

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    abbr = models.CharField(max_length=5)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Country"
+        verbose_name_plural = "Countries"
+        db_table = 'countries'
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True, null=True, unique=True)
+    country = models.ForeignKey(to=Country, on_delete=models.CASCADE)
+    avatar = models.ImageField(blank=True, upload_to='avatars')
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+        db_table = 'profiles'
+
+
+class Device(models.Model):
+    DEVICE_TYPE_CHOICES = ((1, 'web'), (2, 'ios'), (3, 'android'), (4, 'pc'))
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='devices', on_delete=models.CASCADE)
+    device_uuid = models.UUIDField('device UUID', null=True)
+    last_login = models.DateTimeField('last login date', null=True, auto_now=True)
+    device_type = models.PositiveSmallIntegerField(choices=DEVICE_TYPE_CHOICES, default=1)
+    device_os = models.CharField('device os', max_length=20, blank=True)
+    device_model = models.CharField('device model', max_length=50, blank=True)
+    app_version = models.CharField('app version', max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'user_devices'
+        verbose_name = 'Device'
+        verbose_name_plural = 'Devices'
