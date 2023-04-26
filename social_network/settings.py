fa1c19b4e671647eb0c33638f1902ec62c94c3ff
mahdibagheri <bagheri.almas@gmail.com>
2023-04-26 21:50:32 +0330

    add accounts model to INSTALLED APP

--- social_network/settings.py
+++ social_network/settings.py
@@ -13,8 +13,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'accounts.apps.AccountsConfig',
     'rest_framework',
     'rest_framework_simplejwt',
+
 
 ]
 
