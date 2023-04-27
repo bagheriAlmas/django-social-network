6beaf0f6273a8f1333da5ce29ab66f1c947d2437
mahdibagheri <bagheri.almas@gmail.com>
2023-04-27 16:10:32 +0330

    add Post and PostFile to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, PostFile
 
-# Register your models here.
+
+class PostFileInline(admin.TabularInline):
+    model = PostFile
+    fields = ['file', ]
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ['title', 'is_active', 'is_public', 'created_at', 'updated_at']
+    inlines = [PostFileInline, ]
+
+
+admin.site.register(Post, PostAdmin)
