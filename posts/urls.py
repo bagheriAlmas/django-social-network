73756cfa48c2497ddf2e394229d1463ceaef4ad3
mahdibagheri <bagheri.almas@gmail.com>
2023-04-27 22:59:02 +0330

    add urls for apis

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import PostView,PostListView
 
 urlpatterns = [
-
+    path('post/', PostView.as_view(), name='post'),
+    path('post/<int:pk>/', PostView.as_view(), name='post-detail'),
+    path('posts/', PostListView.as_view(), name='post-list'),
 ]
