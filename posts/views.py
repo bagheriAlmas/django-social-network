dec8a2f5b7abfd517729fee5cf175704ee927854
mahdibagheri <bagheri.almas@gmail.com>
2023-04-27 22:58:36 +0330

    - create PostView class for create post and retrieve post detail - create PostListView class for retrieve post list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from posts.models import Post
+from posts.serializers import PostSerializer
+
 
 # Create your views here.
+class PostView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        print(request.user)
+        try:
+            post = Post.objects.get(pk=pk, user=request.user)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self, request):
+        print(request.user)
+        print(request.data)
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostListView(APIView):
+    def get(self, request):
+        posts = Post.objects.filter(is_active=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
