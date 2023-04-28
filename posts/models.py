83e1405a47bc4605b0c2683852db06c5835a0669
mahdibagheri <bagheri.almas@gmail.com>
2023-04-28 15:32:50 +0330

    create Comment Model and Like Model

--- posts/models.py
+++ posts/models.py
@@ -19,6 +19,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
 class PostFile(models.Model):
     post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
     file = models.FileField(upload_to='files')
@@ -32,3 +33,36 @@ class PostFile(models.Model):
 
     def __str__(self):
         return self.file.name
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.PROTECT, related_name='comments')
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField(max_length=200)
+    is_approved = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+        db_table = 'comments'
+
+    def __str__(self):
+        return self.text
+
+
+class Like(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.PROTECT, related_name='likes')
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_liked = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Like'
+        verbose_name_plural = 'Likes'
+        db_table = 'likes'
+
+    def __str__(self):
+        return self.post.title
