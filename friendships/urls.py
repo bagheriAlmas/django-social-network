c24678683aa5e75110004b32731a50d7b5ee9792
mahdibagheri <bagheri.almas@gmail.com>
2023-04-28 19:55:36 +0330

    Add AcceptView URL

--- friendships/urls.py
+++ friendships/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UserListView, RequestView, RequestListView
+from .views import UserListView, RequestView, RequestListView, AcceptView
 
 urlpatterns = [
     path('users/', UserListView.as_view(), name='users-list'),
     path('request/', RequestView.as_view(), name='request'),
     path('requests-list/', RequestListView.as_view(), name='request-list'),
-    # path('accept/'),
+    path('accept/', AcceptView.as_view(), name='accept'),
     # path('friends/'),
 ]
