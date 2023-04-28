2dc47c4eb3ebf491cedc2d7fce276376f2f4116e
mahdibagheri <bagheri.almas@gmail.com>
2023-04-28 19:29:30 +0330

    update urls in urls.py

--- friendships/urls.py
+++ friendships/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import UserListView, RequestView
 
 urlpatterns = [
-
+    path('users/', UserListView.as_view(), name='users-list'),
+    path('request/',RequestView.as_view(), name='request'),
+    # path('requests-list/'),
+    # path('accept/'),
+    # path('friends/'),
 ]
