cf067564875986f56de1dd2ddfea8f8229504e69
mahdibagheri <bagheri.almas@gmail.com>
2023-04-28 16:07:01 +0330

    create model Friendship

--- friendships/models.py
+++ friendships/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Friendship(models.Model):
+    request_from = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='friend_request_from',
+                                     on_delete=models.PROTECT)
+    request_to = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='friend_request_to',
+                                   on_delete=models.PROTECT)
+    is_accepted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Friendship'
+        verbose_name_plural = 'Friendships'
+        db_table = 'friendships'
+        unique_together = ('request_from', 'request_to')
