f1dca989c211c8ac64328a97d00f4502fd81ca01
mahdibagheri <bagheri.almas@gmail.com>
2023-04-28 16:17:47 +0330

    add friendship model to admin panel

--- friendships/admin.py
+++ friendships/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Friendship
 
-# Register your models here.
+
+class FriendshipAdmin(admin.ModelAdmin):
+    model = Friendship
+    list_display = ('request_from', 'request_to', 'is_accepted', 'created_at')
+
+
+admin.site.register(Friendship, FriendshipAdmin)
