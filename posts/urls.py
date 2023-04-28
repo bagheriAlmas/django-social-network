611e5840a9f6b147055d9382509aa90abe81a658
mahdibagheri <bagheri.almas@gmail.com>
2023-04-28 15:32:11 +0330

    add commentView and LikeView to urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostView,PostListView
+from .views import PostView, PostListView, CommentView, LikeView
 
 urlpatterns = [
     path('post/', PostView.as_view(), name='post'),
     path('post/<int:pk>/', PostView.as_view(), name='post-detail'),
     path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/comments/', CommentView.as_view(), name='comment'),
+    path('posts/<int:pk>/likes/', LikeView.as_view(), name='like'),
 ]
