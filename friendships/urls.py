afca28c61fe0b4c22a6a51eb72d82ca7be41acb1
mahdibagheri <bagheri.almas@gmail.com>
2023-04-28 20:17:07 +0330

    add FriendsListView to url

--- friendships/urls.py
+++ friendships/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UserListView, RequestView, RequestListView, AcceptView
+from .views import UserListView, RequestView, RequestListView, AcceptView, FriendListView
 
 urlpatterns = [
     path('users/', UserListView.as_view(), name='users-list'),
     path('request/', RequestView.as_view(), name='request'),
     path('requests-list/', RequestListView.as_view(), name='request-list'),
     path('accept/', AcceptView.as_view(), name='accept'),
-    # path('friends/'),
+    path('friends/', FriendListView.as_view(), name='friends'),
 ]
