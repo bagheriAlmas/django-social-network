167d6622e7862be47fa5c7a21c3568655a9c84a9
mahdibagheri <bagheri.almas@gmail.com>
2023-10-11 19:32:00 +0330

    Add Comment model as a inline model for Post

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, PostFile
+from .models import Post, PostFile, Comment
 
 
 class PostFileInline(admin.TabularInline):
@@ -8,10 +8,16 @@ class PostFileInline(admin.TabularInline):
     extra = 0
 
 
+class PostCommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ['user', 'text', 'is_approved']
+    extra = 0
+
+
 class PostAdmin(admin.ModelAdmin):
     model = Post
-    list_display = ['pk','title', 'is_active', 'is_public', 'created_at', 'updated_at']
-    inlines = [PostFileInline, ]
+    list_display = ['pk', 'title', 'is_active', 'is_public', 'created_at', 'updated_at']
+    inlines = [PostFileInline, PostCommentsInline]
 
 
 admin.site.register(Post, PostAdmin)
